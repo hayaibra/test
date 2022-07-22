4ff67349686394ce717a3608f08c7d3e500dc792
hayaIbra <hayaibra217@gmail.com>
2022-07-22 20:55:09 +0300

    ..

--- src/components/frame/Style.js
+++
@@ -1,9 +0,0 @@
-import { Box, Stack, styled } from "@mui/material";
-
-export const FrameBox = styled(Stack)(({theme})=>({
-  // width: "715px",
-  // height: "768px",
-  // border: "1px solid rgba(191, 149, 63, 0.51)",
-  // backgroundColor: "#f5efe2",
-  // zIndex: -1,
-}));

763d9a486bf5ec26d9eb0d8ec37cacca012826ef
hayaIbra <hayaibra217@gmail.com>
2022-07-22 20:54:28 +0300

    ..

--- src/components/appbar/Appbar.js
+++ src/components/appbar/Appbar.js
@@ -2,7 +2,12 @@ import { AppBar, Avatar, Box, Stack } from "@mui/material";
 import React, { useState } from "react";
 import NavbarGroup from "../sidebar/navbargroup/NavbarGroup";
 import Sidebar from "../sidebar/Sidebar";
-import { SignBox, SignImageBox, SignTitle } from "../sidebar/Style";
+import {
+  SidebarContent,
+  SignBox,
+  SignImageBox,
+  SignTitle,
+} from "../sidebar/Style";
 import { ImageBox, ToolbarBox } from "./Style";
 
 const Appbar = () => {
@@ -44,24 +49,8 @@ const Appbar = () => {
           <Avatar sx={{ bgcolor: "#D9D9D9", width: "40px", height: "40px" }} />
         </Box>
         {open && (
-          <Box
+          <SidebarContent
             sx={{
-              // position: "fixed",
-              height: "100vh",
-              // flexBasis: "209px",
-              borderRadius: "0 16px 16px 0",
-              borderWidth: "0 1px 1px 0 ",
-              borderRightColor: "#BF953F",
-              borderBottomColor: "#BF953F",
-              borderStyle: "solid",
-              // zIndex: 2,
-              backgroundColor: "#FEFEFE",
-              // display: "flex",
-              borderLeft: "1px solid rgba(191, 149, 63, 0.51)",
-              width: "208px",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
               position: "absolute",
               left: "0",
               top: "80px",
@@ -74,7 +63,7 @@ const Appbar = () => {
               </SignImageBox>
               <SignTitle>Sign out</SignTitle>
             </SignBox>
-          </Box>
+          </SidebarContent>
         )}
       </ToolbarBox>
     </AppBar>

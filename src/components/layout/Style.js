893500789557556ada319032edb000bd3fdf67d3
hayaIbra <hayaibra217@gmail.com>
2022-07-22 20:31:39 +0300

    ..

--- src/components/layout/Style.js
+++ src/components/layout/Style.js
@@ -1,10 +1,10 @@
-import { Stack, styled } from "@mui/material";
+import { Stack, styled, Box } from "@mui/material";
 
-// export const LayoutBox = styled(Box)({
-  
-
-  // height: "768px",
-  // position: "absolute",
-  // backgroundColor: "#f5efe2",
-  // flexWrap: "wrap",
-// });
+export const LayoutBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  flexdirection: "row",
+  backgroundColor: "#f5efe2",
+  [theme.breakpoints.only("xs")]: {
+    flexdirection: "column",
+  },
+}));
